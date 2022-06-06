84663f760ac76ef540fc4bb943180b8825b15d5f
“ADRIENNE <adrienneklee@gmail.com>
2022-06-06 16:29:01 -0400

    sign in and log out functionality added

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,11 @@
 class ApplicationController < ActionController::Base
+    before_action :set_current_user
+
+    def set_current_user
+      # flash.now[:notice] = "Logged in successfully"
+      # flash.now[:alert] = "Invalid email or password"
+      if session[:user_id]
+        Current.user = User.find_by(id: session[:user_id])
+      end
+   end 
 end
