990272db4f6e2bf837554d90cf73419a229762f3
“ADRIENNE <adrienneklee@gmail.com>
2022-06-13 14:57:33 -0400

    reset password added

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,5 +7,10 @@ class ApplicationController < ActionController::Base
       if session[:user_id]
         Current.user = User.find_by(id: session[:user_id])
       end
-   end 
+    end 
+
+    def require_user_logged_in!
+      redirect_ to sign_in_path, alert: "you must be signed in to do that." if Current.user.nil?
+    end
+  
 end
